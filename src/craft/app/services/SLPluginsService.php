28459cc46c36c5cc7e17765eefbdbd7567667185
Alvin Yim <alvin.yp.yim@gmail.com>
2018-02-08 13:59:36 +1030

    Move the craft logic to the individual projects

--- src/craft/app/services/SLPluginsService.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Craft;
-
-class SLPluginsService extends PluginsService {
-  public function hasPluginVersionNumberChanged(BasePlugin $plugin) {
-    return false;
-  }
-}
--- src/craft/config/rediscache.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-  'hostname' => $_ENV['REDIS_HOST'],
-  'port' => intval($_ENV['REDIS_PORT']),
-  'database' => intval($_ENV['REDIS_DATABASE']),
-);
